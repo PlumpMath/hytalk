fd7cbf34c5e4e87c4faaa6e166b4498cdd978c4f
Paul Tagliamonte <paultag@debian.org>
2013-01-08 21:04:02 -0500

    magic shit, mang

--- app.hy
+++ app.hy
@@ -8,9 +8,9 @@
 (import-from pygments.lexers PythonLexer ClojureLexer)
 (import-from pygments.formatters HtmlFormatter)
 
-;(import-from hy.compiler.ast27 forge-ast)
-;(import-from hy.lex.tokenize tokenize)
-;(import codegen)
+(import-from hy.compiler.ast27 forge-ast)
+(import-from hy.lex.tokenize tokenize)
+(import codegen)
 
 (def lexers {"python" (PythonLexer)
              "lisp"   (ClojureLexer)})
@@ -21,11 +21,33 @@
 
 (decorate-with (.route app "/")
   (defn index []
-    " Entry point "
+    "Entry point"
     (render-template "slides.html")))
+
+
+(defn colorize-python [x]
+  (highlight x (index lexers "python") (HtmlFormatter)))
+
+
+(defn hy-to-py [hython]
+  (.to_source codegen
+    (forge-ast "stdin" (tokenize hython))))
 
 
 (decorate-with (kwapply (.route app "/format/<language>") {"methods" ["POST"]})
   (defn format-code [language]
-    " Language HTML Formatter "
-    (highlight (index request.form "code") (index lexers language) (HtmlFormatter))))
+    "Language HTML Formatter"
+    (highlight
+      (index request.form "code") (index lexers language) (HtmlFormatter))))
+
+
+(decorate-with (kwapply (.route app "/hy2py") {"methods" ["POST"]})
+  (defn translate-code []
+    "Pythonic converter"
+    (hy-to-py (index request.form "code"))))
+
+
+(decorate-with (kwapply (.route app "/hy2pycol") {"methods" ["POST"]})
+  (defn translate-code-with-color []
+    "Pythonic converter"
+    (colorize-python (hy-to-py (index request.form "code")))))
--- static/hyjack.js
+++ static/hyjack.js
@@ -2,7 +2,6 @@ function fmt_code(cls, typ) {
     $(cls).each(function (i) {
         var node = $(this),
             input = node.html();
-
         node.load('/format/' + typ, {
             "code": input
         });
@@ -10,7 +9,71 @@ function fmt_code(cls, typ) {
 }
 
 
+function reload(input, output) {
+    var input = input.val();
+    output.load(
+        '/hy2pycol',
+        {"code": input}
+    );
+}
+
+
+function reload(node) {
+    var input = node.children(".repl-input"),
+        output = node.children(".repl-output");
+
+    var input = input.val();
+    output.load(
+        '/hy2pycol',
+        {"code": input}
+    );
+}
+
+
+function create_repl() {
+    $(".repl").each(function (i) {
+        var node = $(this),
+            count = 0,
+            pval = node.html(),
+            tara;
+
+        node.html("");
+        node.append("<textarea class = 'repl-input repl-pane repl-left' >" +
+            pval + "</textarea>");
+        node.append("<div class = 'repl-output repl-pane repl-right'></div>");
+
+        tara = node.children(".repl-input");
+
+        // tara.keypress(function(e) {
+        //     console.log(e);
+        //     tara.append(e.keyCode);
+        //     return false;
+        // });
+
+        node.keyup(function(e) {
+            var curcount;
+            count = count + 1;
+            curcount = count;
+
+            window.setTimeout(
+                function() {
+                    if ( curcount == count ) {
+                        reload(node);
+                    } else {
+                        /* Pass. User is still typing */
+                    }
+                },
+                500
+            );
+            return false;
+        });
+        reload(node);
+    });
+}
+
+
 $(document).ready(function() {
     fmt_code(".lisp", "lisp");
     fmt_code(".python", "python");
-})
+    create_repl();
+});
